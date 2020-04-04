86b1283832f92ccbc2624ddf62d9a95f40830c04
Capputch <Capputch@users.noreply.github.com>
2020-04-04 23:00:25 +0100

    Initial commit

---
+++ event.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("submitBtn").addEventListener("click", function(){ 
+        alert("Thank you for sending me a message, I will get back to you as soon as I can. \n -Françoise");
+    } );
+});
