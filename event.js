0943266a80d18425a9f14576ba4aab1255458cc6
Capputch <Capputch@users.noreply.github.com>
2020-04-05 17:20:13 +0100

    up to date version

--- event.js
+++ event.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("submitBtn").addEventListener("click", function(){ 
-        alert("Thank you for sending me a message, I will get back to you as soon as I can. \n -Françoise");
+        alert("Sorry, this functionality isn't available yet.");
     } );
 });
+
+// Thank you for sending me a message, I will get back to you as soon as I can. \n -Françoise
